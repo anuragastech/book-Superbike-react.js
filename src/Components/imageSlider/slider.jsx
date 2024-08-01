88900c76b73762edbd9638852862ee4114f08bf6
Anurag <anuragraveendren98@gmail.com>
2024-08-02 02:47:37 +0530

    addslider

---
+++ src/Components/imageSlider/slider.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./slider.css"
+
+
+const Slider = () => {
+    return (
+        <div className='slider'>
+            <div className='slider-left'>
+<h2>NEW ARRIVALS ONLY</h2>
+<div>
+    <div className='hand-hand-icon'>
+<p>new</p>
+<img src={} alt=''/>
+    </div>
+    <p>collections</p>
+    <p>for everyone</p>
+</div>
+
+<div className='slidrt-latest-btn'>
+    <div>Latest collection</div>
+    <img src='' alt=''/>
+
+</div>
+            </div>
+            <div className='slider-right'>
+            <img src='' alt=''/>
+
+
+</div>
+        </div>
+    );
+}
+
+export default Slider;
