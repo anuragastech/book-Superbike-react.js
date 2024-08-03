a48c0fb8669c43d1d4bcb85e8fbafb770d2759f5
Anurag <anuragraveendren98@gmail.com>
2024-08-03 11:13:39 +0530

    productsadded

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import Navbar from "./Components/Navbar/navebar"
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import shop from "./Components/pages/shop"
-// import ShopCAtegory from './Components/pages/shopCAtegory';
+import Navbar from "./Components/Navbar/navebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Components/pages/shop";
+// import ShopCategory from './Components/pages/shopCategory';
 import Product from './Components/pages/product';
 import Cart from './Components/pages/cart';
 import LoginSignUp from './Components/pages/LoginSignUp';
-import Slider from "./Components/imageSlider/slider"
-import Item from './Components/item/item';
+import Slider from "./Components/imageSlider/slider";
+import Collection from "./Components/imagecollection/collection";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-   <BrowserRouter>
-   <Navbar/>
-   <Routes>
-    <Route path='/' element={<shop/>}/>
-    {/* <Route path='/contact' element={<ShopCAtegory/>}/> */}
-    <Route path='/product' element={<Product/>}/>
-    <Route path=':productId' element={<Product/>}/>
-    <Route path='/cart' element={<Cart/>}/>
-    <Route path='/login' element={<LoginSignUp/>}/>
-
-
-
-<Route/>
-
-
-       </Routes>
-
-   
-   </BrowserRouter>
-   <Slider/>
-   <Item/>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Shop />} />
+            {/* <Route path='/contact' element={<ShopCategory />} /> */}
+            <Route path='/product' element={<Product />} />
+            <Route path='/product/:productId' element={<Product />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/login' element={<LoginSignUp />} />
+          </Routes>
+        </BrowserRouter>
+        <Slider />
+        <Collection />
       </div>
     );
   }
--- src/Components/imagecollection/collection.jsx
+++ src/Components/imagecollection/collection.jsx
@@ -1,24 +1,58 @@
-
-// ParentComponent.js
 import React from 'react';
-import Item from './Item';
+import Item from '../item/item';
+import "./products.css"
 
 const ParentComponent = () => {
-    const itemData = {
-        image: 'https://via.placeholder.com/150', // Replace with actual image URL
-        name: 'Sample Item',
-        new_price: '$10.00',
-        old_price: '$20.00'
-    };
+    const itemData = [
+        {
+            image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg',
+            name: 'Sample Item 1',
+            new_price: '$10.00',
+            old_price: '$20.00'
+        },
+        {
+            image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg',
+            name: 'Sample Item 2',
+            new_price: '$15.00',
+            old_price: '$30.00'
+        },
+        {
+            image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg',
+            name: 'Sample Item 3',
+            new_price: '$20.00',
+            old_price: '$40.00'
+        },
+        {
+            image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg',
+            name: 'Sample Item 4',
+            new_price: '$25.00',
+            old_price: '$50.00'
+        },
+        {
+            image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg',
+            name: 'Sample Item 5',
+            new_price: '$30.00',
+            old_price: '$60.00'
+        },
+        {
+            image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg',
+            name: 'Sample Item 6',
+            new_price: '$35.00',
+            old_price: '$70.00'
+        }
+    ];
 
     return (
         <div className='parent'>
-            <Item 
-                image={itemData.image} 
-                name={itemData.name} 
-                new_price={itemData.new_price}
-                old_price={itemData.old_price}
-            />
+            {itemData.map((item, index) => (
+                <Item 
+                    key={index}
+                    image={item.image} 
+                    name={item.name} 
+                    new_price={item.new_price}
+                    old_price={item.old_price}
+                />
+            ))}
         </div>
     );
 }
